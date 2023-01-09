57fd030ded981f6b1670e4f029a53660e074e86c
Modzie <modzielol@gmail.com>
2023-01-09 10:12:32 +0100

    sah

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:04:45 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/01/05 14:43:12 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/01/09 10:12:12 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,19 @@ void	is_a_map(char *argv)
 	int	i;
 
 	i = 0;
-	while (argv[i] && argv[i] != '.')
-		i++;
-	i++;
-	if (argv[i] == 'b' && argv[i + 1] == 'e' && argv[i + 2] == 'r'
-		&& argv[i + 3] == '\0')
-		return ;
-	// while (argv[i])
-	// {
-	// 	if (argv[i] == '.' && argv[i + 1] == 'b' && argv[i + 2] == 'e'
-	// 		&& argv[i + 3] == 'r' && argv[i + 4] == '\0')
-	// 		return ;
+	// while (argv[i] && argv[i] != '.')
 	// 	i++;
-	// }
+	// i++;
+	// if (argv[i] == 'b' && argv[i + 1] == 'e' && argv[i + 2] == 'r'
+	// 	&& argv[i + 3] == '\0')
+		return ;
+	while (argv[i])
+	{
+		if (argv[i] == '.' && argv[i + 1] == 'b' && argv[i + 2] == 'e'
+			&& argv[i + 3] == 'r' && argv[i + 4] == '\0')
+			return ;
+		i++;
+	}
 	display_error(7);
 }
 
