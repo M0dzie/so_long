8ac8347d0f561fd8d9bbfd60a47edfdd13f1c3f2
Modzie <modzielol@gmail.com>
2023-01-11 11:11:22 +0100

    check error done

--- check_errors.c
+++ check_errors.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 09:43:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/01/11 11:00:24 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/01/11 11:07:59 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ void	check_walls(char **map, int size)
 	while (size > 1)
 	{
 		len = ft_strlen(map[size]);
-		if (map[size - 1][0] != '1' && map[size - 1][len] != '1')
+		if (map[size - 1][0] != '1' || map[size - 1][len - 1] != '1')
 			display_error(2);
 		size--;
 	}
